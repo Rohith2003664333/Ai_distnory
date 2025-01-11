21588d6255538c486e6417bf6b8c417accc4fc08
Rohith H <146511008+Rohith2003664333@users.noreply.github.com>
2025-01-11 12:11:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,5 +95,4 @@ def record_message():
     return jsonify({"transcription": transcription})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Default to 8000
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
